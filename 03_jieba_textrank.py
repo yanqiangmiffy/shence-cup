970959fe3bd2a105fe46fc1f1df11a59a23ddfe2
yanqiangmiffy <1185918903@qq.com>
2018-08-31 14:06:38 +0800

    wenjia

--- 03_jieba_textrank.py
+++ 03_jieba_textrank.py
@@ -7,63 +7,37 @@ from jieba.analyse import *
 import jieba.analyse
 from tqdm import tqdm
 jieba.analyse.set_stop_words('data/stop_words.txt')
-
+import random
 # 加载数据
 all_docs_file=open('data/all_docs.txt','r',encoding='utf-8')
 
-def extract_keyword_by_tfidf():
 
-    ids=[]
-    labels_1=[]
-    labels_2=[]
-    for line in tqdm(all_docs_file):
-        data=line.strip().split('')
-        ids.append(data[0])
-        text=data[1]+data[2]
-        keywords=[keyword for keyword, weight in extract_tags(text, withWeight=True,topK=5)]
-        if len(keywords)>=2:
-            labels_1.append(keywords[0])
-            labels_2.append(keywords[1])
-        else:
-            print(text)
-            labels_1.append(keywords[0])
-            labels_2.append('')
-
-    import csv
-    with open('result/02_snow_tfidf.csv','w',encoding='utf-8',newline='') as out_data:
-        csv_writer=csv.writer(out_data)
-        csv_writer.writerow(('id','label1','label2'))
-        for id,label1,label2 in zip(ids,labels_1,labels_2):
-            csv_writer.writerow((id,label1,label2))
-
-    df_data=pd.DataFrame([ids,labels_1,labels_2],columns=['id','label1','label2'])
-    df_data.to_csv('result/02_jieba_tfidf.csv',index=False)
-
-
+# 基于jieba的textrank提取关键词
 def extract_keyword_by_textrank():
-
     ids=[]
     labels_1=[]
     labels_2=[]
     for line in tqdm(all_docs_file):
         data=line.strip().split('')
         ids.append(data[0])
-        text=data[1]+data[2]
+        text=data[1]+data[2] #标题加文章
+        # text=data[1] #标题
         keywords=[keyword for keyword, weight in textrank(text, withWeight=True,topK=5)]
         if len(keywords)>=2:
             labels_1.append(keywords[0])
             labels_2.append(keywords[1])
-        else:
-            print(text)
+        elif len(keywords)==1:
             labels_1.append(keywords[0])
             labels_2.append('')
+        else:
+            words=[word for word in jieba.cut(data[1])]
+            labels_1.append(words[0])
+            labels_2.append(random.choice(words))
+    data = {'id': ids,
+            'label1': labels_1,
+            'label2': labels_2}
+    df_data = pd.DataFrame(data, columns=['id', 'label1', 'label2'])
+    df_data.to_csv('result/03_jieba_textrank.csv', index=False)
 
-    import csv
-    with open('result/02_snow_tfidf.csv','w',encoding='utf-8',newline='') as out_data:
-        csv_writer=csv.writer(out_data)
-        csv_writer.writerow(('id','label1','label2'))
-        for id,label1,label2 in zip(ids,labels_1,labels_2):
-            csv_writer.writerow((id,label1,label2))
 
-    df_data=pd.DataFrame([ids,labels_1,labels_2],columns=['id','label1','label2'])
-    df_data.to_csv('result/02_jieba_tfidf.csv',index=False)
+extract_keyword_by_textrank()
--- demo.py
+++ demo.py
@@ -2,3 +2,17 @@
 # @Author  : quincyqiang
 # @File    : demo.py
 # @Time    : 2018/8/31 10:12
+from snownlp import SnowNLP
+import pandas as pd
+text="《命运速递》主题曲MV曝光吕晓霖片中虐恋触人心弦"
+snow=SnowNLP(text)
+keyword=snow.keywords(limit=5)
+print(keyword[:2])
+print(snow.words)
+
+ids,labels_1,labels_2=[[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]]
+data={'id':ids,
+      'label1':labels_1,
+      'label2':labels_2}
+df_data=pd.DataFrame(data,columns=['id','label1','label2'])
+df_data.to_csv('result/01_textrank1.csv',index=False)
