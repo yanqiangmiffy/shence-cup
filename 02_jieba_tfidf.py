d372703e568743f7166c741b3a5dee50d0bb7844
yanqaingmiffy <1185918903@qq.com>
2018-09-01 22:54:10 +0800

    添加自定义词典

--- 02_jieba_tfidf.py
+++ 02_jieba_tfidf.py
@@ -7,7 +7,7 @@ from jieba.analyse import *
 import jieba.analyse
 from tqdm import tqdm
 jieba.analyse.set_stop_words('data/stop_words.txt')
-
+jieba.load_userdict('data/custom_dict.txt')
 # 加载数据
 all_docs_file=open('data/all_docs.txt','r',encoding='utf-8')
 
@@ -17,7 +17,7 @@ def extract_keyword_by_tfidf():
     labels_1=[]
     labels_2=[]
     for line in tqdm(all_docs_file):
-        data=line.strip().split('')
+        data=line.strip().split('\001')
         ids.append(data[0])
         text=data[1]+data[2]
         keywords=[keyword for keyword, weight in extract_tags(text, withWeight=True,topK=5)]
@@ -36,6 +36,8 @@ def extract_keyword_by_tfidf():
             'label2': labels_2}
     df_data = pd.DataFrame(data, columns=['id', 'label1', 'label2'])
     df_data.to_csv('result/02_jieba_tfidf.csv', index=False)
+    all_docs_file.close()
 
 
-
+if __name__ == '__main__':
+    extract_keyword_by_tfidf()
