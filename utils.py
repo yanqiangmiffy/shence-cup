6f57b8c3062bb25aa40a2dcdb71b8ab374d912d0
yanqaingmiffy <1185918903@qq.com>
2018-09-01 22:53:47 +0800

    测试军事

--- utils.py
+++ utils.py
@@ -3,5 +3,19 @@
 # @File    : utils.py
 # @Time    : 2018/8/31 15:23
 
-data=[1,2,3,45,5]
-print(data[3])
+import jieba.posseg
+# text="华为新机皇P30pro曝光"
+# text="【创新菜】法式红酒烩鸡肉,"
+# text="【菜谱】夏日美味小点心——绿豆冰糕"
+# text="老公做了一桌菜，色香味俱全，婆婆一回来，看见就不高兴"
+text='"巨舰,航母",我海军一款4万吨巨舰作用比肩航母未来至少需要12艘'
+for word,tag in jieba.posseg.cut(text):
+    print(word,tag)
+
+# sen='UPDATE staff_table SET dept="Market" WHERE where dept="IT"' # 提取引号中的内容
+# import re
+# mth=re.findall('"(.*?)"',sen)
+# for m in mth:
+#     print(m)
+#
+# print('我\001爱你')
