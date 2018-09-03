242541ae6eac624a6ceec6cc2fd3d17f0a7f26a4
yanqiangmiffy <1185918903@qq.com>
2018-09-03 14:23:38 +0800

    util

--- 06_jieba_ensemble.py
+++ 06_jieba_ensemble.py
@@ -6,7 +6,7 @@
 
 import pandas as pd
 from tqdm import tqdm
-from jieba.analyse import extract_tags # tf-idf
+from jieba.analyse import extract_tags,textrank # tf-idf
 from jieba import posseg
 import random
 import jieba
@@ -28,22 +28,22 @@ jieba.load_userdict('data/custom_dict.txt') # 设置词库
 
 '''
 
-all_docs_file=open('data/all_docs.txt','r',encoding='utf-8')
+test_data=pd.read_csv('data/test_docs.csv')
+train_data=pd.read_csv('data/new_train_docs.csv')
 allow_pos={'nr':1,'nz':2,'ns':3,'nt':4,'eng':5,'l':6,'i':7,'n':8,'a':9,'nrt':10,'v':11,'t':12}
-train_docs_file=open('data/train_docs_keywords.txt','r',encoding='utf-8')
 
 
-def extract_keyword_ensemble():
-    ids = []
+def extract_keyword_ensemble(test_data):
+
+    ids,titles,docs=test_data['id'],test_data['title'],test_data['doc']
+
     labels_1 = []
     labels_2 = []
     empty=0
-    for line in tqdm(all_docs_file):
+    for data in tqdm(zip(titles,docs)):
         keywords = []
-        data = line.strip().split('\001')
-        ids.append(data[0])
 
-        word_tags=[(word,pos) for word,pos in posseg.cut(data[1])] # 标题
+        word_tags=[(word,pos) for word,pos in posseg.cut(data[0])] # 标题
         for word_pos in word_tags:
             if word_pos[1] in allow_pos:
                 keywords.append(word_pos)
@@ -55,14 +55,13 @@ def extract_keyword_ensemble():
         if len(keywords) <2:
             # 使用tf-idf
             empty+=1
-            temp_keywords = [keyword for keyword, weight in extract_tags(data[1]+data[2], withWeight=True, topK=5)]
+            temp_keywords = [keyword for keyword, weight in extract_tags(data[0]+str(data[1]), withWeight=True, topK=5)]
             # print("tfidf:",temp_keywords)
             labels_1.append(temp_keywords[0])
             labels_2.append(temp_keywords[1])
         else:
             labels_1.append(keywords[0][0])
             labels_2.append(keywords[1][0])
-    print(empty)
 
     data = {'id': ids,
             'label1': labels_1,
@@ -70,11 +69,62 @@ def extract_keyword_ensemble():
 
     df_data = pd.DataFrame(data, columns=['id', 'label1', 'label2'])
     df_data.to_csv('result/06_jieba_ensemble.csv', index=False)
-    print(empty)
-    # 关闭文件
-    all_docs_file.close()
-    train_docs_file.close()
+    print("使用tf-idf提取的次数：",empty)
+
+all_pos=['ns','vn', 'v','nr','nt','eng','l','i','a','nrt']
+def evaluate(df_data=None):
+    ids, titles, docs = df_data['id'], df_data['title'], df_data['doc']
+    true_keywords=df_data['keyword'].apply(lambda x:x.split(','))
+    labels_1 = []
+    labels_2 = []
+    empty = 0
+    score=0
+    for data in tqdm(zip(titles, docs,true_keywords)):
+        keywords = []
+
+        word_tags = [(word, pos) for word, pos in posseg.cut(data[0])]  # 标题
+        for word_pos in word_tags:
+            if word_pos[1] in allow_pos:
+                keywords.append(word_pos)
+
+        keywords = [keyword for keyword in keywords if len(keyword[0]) > 1]
+        keywords = sorted(keywords, reverse=False, key=lambda x: allow_pos[x[1]])
+        # print(keywords)
+
+        if len(keywords) < 2:
+            # 使用tf-idf
+            empty += 1
+            temp_keywords = [keyword for keyword, weight in
+                             extract_tags(data[0] + str(data[1]), withWeight=True,allowPOS=(),topK=5)]
+            # print("tfidf:",temp_keywords)
+            labels_1.append(temp_keywords[0])
+            labels_2.append(temp_keywords[1])
+            print(temp_keywords[0],temp_keywords[1],data[2])
+            if temp_keywords[0] in data[2]:
+                score+=0.5
+            if temp_keywords[1] in data[2]:
+                score+=0.5
+        else:
+            labels_1.append(keywords[0][0])
+            labels_2.append(keywords[1][0])
+
+            # if keywords[0][0] not in data[2] and keywords[1][0] not in data[2]:
+            #     print(keywords[0][0],keywords[1][0],data[2],data[0],keywords)
+
+            if keywords[0][0] in data[2]:
+                score+=0.5
+            if keywords[1][0] in data[2]:
+                score+=0.5
+
+    data = {'id': ids,
+            'label1': labels_1,
+            'label2': labels_2}
+    df_data = pd.DataFrame(data, columns=['id', 'label1', 'label2'])
+    df_data.to_csv('result/06_train.csv', index=False)
+    print("使用tf-idf提取的次数：", empty)
+    print("最终得分为：",score)
 
 
 if __name__ == '__main__':
-    extract_keyword_ensemble()
+    # extract_keyword_ensemble(test_data)
+    evaluate(df_data=train_data)
--- utils.py
+++ utils.py
@@ -3,15 +3,143 @@
 # @Author  : quincyqiang
 # @File    : utils.py
 # @Software: PyCharm
-custom_dict_file=open('data/custom_dict.txt','r',encoding='utf-8')
-def process():
+import os
+# 基于sklearn tfidf
+import pandas as pd
+from tqdm import tqdm
+from jieba.analyse import extract_tags  # tf-idf
+from jieba import posseg
+import pickle
+import jieba
+jieba.load_userdict('data/custom_dict.txt')  # 设置词库
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+test_data = pd.read_csv('data/test_docs.csv')
+train_data = pd.read_csv('data/new_train_docs.csv')
+
+allow_pos = {'nr': 1, 'nz': 2, 'ns': 3, 'nt': 4, 'eng': 5, 'l': 6, 'i': 7, 'a': 8, 'nrt': 9, 'n': 10, 'v': 11, 't': 12}
+stop_words = open('data/stop_words.txt', 'r', encoding='utf-8').read().split('\n')
+
+
+def generate_docs(data_path):
     """
-    去除长度为1的word
+    标题和文章分句 重要词性组成
     :return:
     """
-    with open('data/perfect.txt','w',encoding='utf-8') as new_file:
-        for line in custom_dict_file:
-            data=line.strip().split(' ')
-            new_file.write(" ".join(data[:2])+"\n")
-if __name__ == '__main__':
-    process()
+    ids, titles, docs = test_data['id'], test_data['title'], test_data['doc']
+    print("generate docs..")
+    all_docs = []
+    for data in tqdm(titles, docs):
+        doc = data[0] + '。' + data[1]
+        word_tags = []
+        for word, pos in posseg.cut(doc):
+            if word not in stop_words and pos in allow_pos:
+                if len(word) > 1:
+                    word_tags.append((word, pos))
+        new_doc = " ".join([word_tag[0] for word_tag in word_tags])
+        # print(new_doc)
+        all_docs.append(new_doc)
+    with open(data_path, 'wb') as out_data:
+        pickle.dump(all_docs, out_data, pickle.HIGHEST_PROTOCOL)
+    return all_docs
+
+
+def load_docs(data_path = 'data/all_doc_pos.pkl'):
+    """
+    加载 分词后的文档
+    :return:
+    """
+    if os.path.exists(data_path):
+        with open(data_path, 'rb') as in_data:
+            all_docs = pickle.load(in_data)
+        return all_docs
+
+    all_docs=generate_docs(data_path)
+    return all_docs
+
+
+def sort_coo(coo_matrix):
+    """
+    排序
+    :param coo_matrix:
+    :return:
+    """
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    """
+    获取关键字
+    :param feature_names:
+    :param sorted_items:
+    :param topn:
+    :return:
+    """
+    sorted_items = sorted_items[:topn]
+
+    score_vals = []
+    feature_vals = []
+
+    for idx, score in sorted_items:
+
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+
+    results = {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]] = score_vals[idx]
+
+    return results
+
+
+def train(model_path):
+    """
+    训练并返回模型
+    :return:
+    """
+    documents = load_docs()
+    print("train....")
+    cv = CountVectorizer(max_df=0.85, min_df=1, stop_words=stop_words)
+    word_count_vector = cv.fit_transform(documents)
+
+    # 计算tfidf
+    tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
+    tfidf_transformer.fit(word_count_vector)
+
+    with open(model_path, 'wb') as out_data:
+        pickle.dump(cv, out_data, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(tfidf_transformer, out_data, pickle.HIGHEST_PROTOCOL)
+    return cv, tfidf_transformer
+
+
+def load_model(model_path='model/cv_tfidf.pkl'):
+    """
+    加载训练好的模型
+    :param model_path:
+    :return:
+    """
+    if os.path.exists(model_path):
+        with open(model_path, 'rb') as in_data:
+            cv = pickle.load(in_data)
+            tfidf_transformer = pickle.load(in_data)
+        return cv, tfidf_transformer
+
+    cv, tfidf_transformer = train(model_path)
+    return cv, tfidf_transformer
+
+
+def get_keywords(cv, tfidf_transformer, doc, feature_names):
+    tf_idf_vector = tfidf_transformer.transform(cv.transform([doc]))
+
+    sorted_items = sort_coo(tf_idf_vector.tocoo())
+
+    keywords = extract_topn_from_vector(feature_names, sorted_items, 10)
+
+    print(keywords)
+    # for k in keywords:
+    #     print(k, keywords[k])
+
+    keywords = list(keywords.keys())
+
+    return keywords
