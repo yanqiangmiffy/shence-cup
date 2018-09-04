ec3712b53f49ead7556b1ea234e68b530a42b506
yanqiangmiffy <1185918903@qq.com>
2018-09-04 17:13:24 +0800

    词频

--- 06_jieba_ensemble.py
+++ 06_jieba_ensemble.py
@@ -31,6 +31,22 @@ jieba.load_userdict('data/custom_dict.txt') # 设置词库
 test_data=pd.read_csv('data/test_docs.csv')
 train_data=pd.read_csv('data/new_train_docs.csv')
 allow_pos={'nr':1,'nz':2,'ns':3,'nt':4,'eng':5,'n':6,'l':7,'i':8,'a':9,'nrt':10,'v':11,'t':12}
+tf_pos = ['ns', 'n', 'vn', 'nr', 'nt', 'eng', 'nrt']
+
+def generate_name(word_tags):
+    name_pos = ['ns', 'n', 'vn', 'nr', 'nt', 'eng', 'nrt']
+    for word_tag in word_tags:
+        if word_tag[0] == '·':
+            index = word_tags.index(word_tag)
+            if (index+1)<len(word_tags):
+                prefix = word_tags[index - 1]
+                suffix = word_tags[index + 1]
+                if prefix[1] in name_pos and suffix[1] in name_pos:
+                    name = prefix[0] + word_tags[index][0] + suffix[0]
+                    word_tags = word_tags[index + 2:]
+                    word_tags.insert(0, (name, 'nr'))
+    return word_tags
+
 
 
 def extract_keyword_ensemble(test_data):
@@ -41,10 +57,13 @@ def extract_keyword_ensemble(test_data):
     labels_2 = []
     empty=0
     for title,doc in tqdm(zip(titles,docs)):
-
         keywords = []
-
         word_tags=[(word,pos) for word,pos in posseg.cut(title)] # 标题
+
+        # 判断是否存在特殊符号
+        if '·' in title:
+            word_tags = generate_name(word_tags)
+
         for word_pos in word_tags:
             if word_pos[1] in allow_pos:
                 keywords.append(word_pos)
@@ -75,7 +94,6 @@ def extract_keyword_ensemble(test_data):
     df_data.to_csv('result/06_jieba_ensemble.csv', index=False)
     print("使用tf-idf提取的次数：",empty)
 
-all_pos=['ns', 'n', 'vn','nr','nt','eng','l','i','a','nrt']
 
 
 def evaluate():
@@ -92,8 +110,11 @@ def evaluate():
         doc=data[1]
 
         keywords = []
-
         word_tags = [(word, pos) for word, pos in posseg.cut(title)]  # 标题
+        # 判断是否存在特殊符号
+        if '·' in title:
+            word_tags=generate_name(word_tags)
+
         for word_pos in word_tags:
             if word_pos[1] in allow_pos:
                 keywords.append(word_pos)
@@ -106,7 +127,7 @@ def evaluate():
             # 使用tf-idf
             use_idf += 1
             temp_keywords = [keyword for keyword in
-                             extract_tags(title + str(doc),topK=2)]
+                             extract_tags(title + str(doc)[:100]+str(doc)[:-80],topK=2)]
             # print("tfidf:",temp_keywords)
             labels_1.append(temp_keywords[0])
             labels_2.append(temp_keywords[1])
@@ -123,16 +144,14 @@ def evaluate():
                 part_wrong+=1
                 # print("prediction--true keys--title--candidate keys")
                 temp_keywords = [keyword for keyword in
-                                 extract_tags(title + str(doc[:100])+str(doc[:-50]),topK=2)]
-                # key_1,key_2=temp_keywords
+                                 extract_tags(title + str(doc),topK=2)]
+
                 print((key_1,key_2),'--',temp_keywords,'--',true_keys,'--',title,'--',keywords)
 
             if key_1 in true_keys:
                 score+=0.5
-
             if key_2 in true_keys:
                 score+=0.5
-
             labels_1.append(key_1)
             labels_2.append(key_2)
 
---
+++ 08_word_counts_ensemble.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Author  : quincyqiang
+# @File    : 08_word_counts_ensemble.py
+# @Time    : 2018/9/4 17:12
